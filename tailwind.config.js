1ec281a84acca6902a52d212b4332e1e2dc0f105
liminote <liminote1977@gmail.com>
2026-02-22 00:47:31 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
- /** @type {import('tailwindcss').Config} */
+  /** @type {import('tailwindcss').Config} */
   export default {
       content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
       plugins: [],
-      theme: {
-          extend: {
-              fontFamily: {
-                  sans: ['"Noto Sans TC"', 'Inter', 'system-ui', 'sans-serif'],
-                  serif: ['"Noto Serif TC"', 'Georgia', 'serif'],
-                  mono: ['"JetBrains Mono"', 'monospace'],
-              },
-          },
-      },
   }
+
